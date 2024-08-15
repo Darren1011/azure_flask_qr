8c6074068bb6e15d836ff61befe9ee429dd60843
Darren1011 <joonhao.yan@gmail.com>
2024-08-15 10:06:30 +0800

    Second Commit

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Navigate to the Flask backend directory and start the server
+cd flask-react-qr
+gunicorn --bind=0.0.0.0 --timeout 600 app:app &
+
+# Navigate to the React frontend directory, install dependencies, build, and serve it
+cd ../qr-frontend
+npm install
+npm run build
+npx serve -s build
